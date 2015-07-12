bc97d008a94bebd4ab7ddd9b5d1d12f7a3f2af91
Alex Lew <alew104@gmail.com>
2015-07-11 20:13:34 -0700

    updated login method

--- js/spotify.js
+++ js/spotify.js
@@ -7,23 +7,26 @@ var data;
 var baseUrl = 'https://api.spotify.com/v1/search?type=playlist&query='
 var myApp = angular.module('myApp', [])
 
-function loginWithSpotify() {
-    var client_id = '62283e59b09041c0b318be64e97ae046';
-    var redirect_uri = 'http://students.washington.edu/alew104/info343/spotify-challenge/index.html';
-    var scopes = 'playlist-modify-public';
 
-    var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
-        '&response_type=token' +
-        '&scope=' + encodeURIComponent(scopes) +
-        '&redirect_uri=' + encodeURIComponent(redirect_uri);
-    document.location = url;
-}
 
 
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http){
   $scope.playlists = {};
   console.log('1')
+
+  $scope.loginWithSpotify = function() {
+      var client_id = '62283e59b09041c0b318be64e97ae046';
+      var redirect_uri = 'http://students.washington.edu/alew104/info343/spotify-challenge/index.html';
+      var scopes = 'playlist-modify-public';
+
+      var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
+          '&response_type=token' +
+          '&scope=' + encodeURIComponent(scopes) +
+          '&redirect_uri=' + encodeURIComponent(redirect_uri);
+          document.location = url;
+  }
+
   $scope.getPlaylists = function() {
     console.log('getting playlists');
     $http.get(baseUrl + $scope.playlist).success(function(response){
