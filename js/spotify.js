cbd24221183d66fbe34d67c5faf69fb5c9da1626
Alex Lew <alew104@gmail.com>
2015-07-11 20:06:11 -0700

    wrong link

--- js/spotify.js
+++ js/spotify.js
@@ -9,7 +9,7 @@ var myApp = angular.module('myApp', [])
 
 function loginWithSpotify() {
     var client_id = '62283e59b09041c0b318be64e97ae046';
-    var redirect_uri = 'http://student.washington.edu/alew104/info343/spotify-challenge/index.html';
+    var redirect_uri = 'http://students.washington.edu/alew104/info343/spotify-challenge/index.html';
     var scopes = 'playlist-modify-public';
 
     var url = 'https://accounts.spotify.com/authorize?client_id=' + client_id +
