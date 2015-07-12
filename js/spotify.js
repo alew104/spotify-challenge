ac86aec3b894e8ffd2f3ab31d3d3fe0904afdf23
Alex Lew <alew104@gmail.com>
2015-07-11 21:49:29 -0700

    dying

--- js/spotify.js
+++ js/spotify.js
@@ -59,7 +59,7 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http){
   $scope.getPlaylists = function() {
     console.log('getting playlists');
     $http.get(baseUrl + $scope.playlist).success(function(response){
-      data = response.playlists.items;
+      data = response;
       console.log(data);
       parseIds(data);
     })
